b79aef2a069220c2ddbfa1dd9a04df92cdbd655b
Mryck <emeric.bombenger@me.com>
2021-10-06 00:16:21 +0200

    lint

--- multitools.py
+++ multitools.py
@@ -1,12 +1,15 @@
 import click
 
+
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
 def hello():
-    click.echo('Hello World!')
+    click.echo("Hello World!")
+
 
 if __name__ == "__main__":
     cli()
