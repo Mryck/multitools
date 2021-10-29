86ec5919c10c8b759e92c04dfbc8c3f25cfef54d
Mryck <emeric.bombenger@me.com>
2021-10-29 19:25:58 +0200

    feat: add wheather (metno)

--- multitools.py
+++ multitools.py
@@ -1,5 +1,6 @@
 import click
 from tools.speedtest import run_speedtest
+from tools.metno import get_weather
 
 
 
@@ -9,6 +10,7 @@ def cli():
 
 
 cli.add_command(run_speedtest, "speedtest")
+cli.add_command(get_weather, "weather")
 
 
 if __name__ == "__main__":
